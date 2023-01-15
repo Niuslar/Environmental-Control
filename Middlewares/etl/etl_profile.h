34f4690ad12b50506eff9cc12ddc235b970f086c
Niuslar <niuslar@outlook.com>
2023-01-15 12:31:07 +0000

    feature: Add ETL to project

---
+++ Middlewares/etl/etl_profile.h
@@ -0,0 +1,15 @@
+/**
+ * @file etl_profile.h
+ *
+ *  Created on: 12 Apr 2022
+ *      Author: niuslar
+ */
+
+#ifndef ETL_PROFILE_H_
+#define ETL_PROFILE_H_
+
+#define ETL_COMPILER_ARM
+
+#define MAX_STRING_SIZE 150
+
+#endif /* ETL_PROFILE_H_ */
