5d1db7f81f10b4913143bf0e7d071e882b4bc6b3
Salavat Magazov <s.magazov@gmail.com>
2023-01-27 16:10:04 +0000

    wip

---
+++ Lib/Inc/CDispatcher.h
@@ -0,0 +1,17 @@
+/*
+ * CDispatcher.h
+ *
+ *  Created on: 24 Jan 2023
+ *      Author: salavat.magazov
+ */
+
+#ifndef CDISPATCHER_H_
+#define CDISPATCHER_H_
+
+class CDispatcher {
+public:
+	CDispatcher();
+	virtual ~CDispatcher();
+};
+
+#endif /* CDISPATCHER_H_ */
---
+++ Lib/Src/CDispatcher.cpp
@@ -0,0 +1,18 @@
+/*
+ * CDispatcher.cpp
+ *
+ *  Created on: 24 Jan 2023
+ *      Author: salavat.magazov
+ */
+
+#include "CDispatcher.h"
+
+CDispatcher::CDispatcher() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CDispatcher::~CDispatcher() {
+	// TODO Auto-generated destructor stub
+}
+
